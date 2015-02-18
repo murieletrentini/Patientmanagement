a299ba60370b2ff78833d92b087611b8b437c18d
Muriele Trentini <muriele@trentini.ch>
2015-02-18 21:23:21 +0100

    improves script.js design

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 /* Created by muriele on 03.01.15. */
-
-var saveButton = document.querySelector('.save');
-var inputName = document.querySelector('.name');
-var inputSurname = document.querySelector('.surname');
-var patientListBodyElement = document.querySelector('#patientlist tbody');
-var inputSearch = document.querySelector('input.search');
-var patientList = document.querySelector('#patientlist table');
-var addAntiBodyButton = document.querySelector('.addAntiBody');
-var removeAntiBodyButton = document.querySelector('.removeAntiBody');
-var patientStorage;
-
-function getPatientsFromStorage () {
-    patientStorage = JSON.parse(localStorage.getItem("storedPatientArray"));
-    if (patientStorage == null) patientStorage = [];
+function getPatientsFromStorage() {
+    var patientStorage = JSON.parse(localStorage.getItem("storedPatientArray"));
+    if (patientStorage == null) {
+        patientStorage = [];
+    }
+    return patientStorage;
 }
 
-getPatientsFromStorage();
+function savePatientsToStorage(patients) {
+    localStorage.setItem("storedPatientArray", JSON.stringify(patients));
+}
 
-function displayStorage() {
-    if (patientStorage.length > 0) {
+var patients = getPatientsFromStorage();
+
+
+function updateTable(patients) {
+    var patientListBodyElement = document.querySelector('#patientlist tbody');
+    if (patients.length > 0) {
         var text = '';
         var i;
-        for (i = 0; i < patientStorage.length; i++) {
-            var currentpat = patientStorage[i];
+        for (i = 0; i < patients.length; i++) {
+            var currentpat = patients[i];
             text += "<tr><td>" + currentpat.surname + "</td>";
             text += "<td>" + currentpat.name + "</td>";
             text += "<td>" + currentpat.sex + "</td>";
@@ -37,8 +35,11 @@ function displayStorage() {
     }
 }
 
-displayStorage();
 
+updateTable(patients);
+
+var addAntiBodyButton = document.querySelector('.addAntiBody');
+var removeAntiBodyButton = document.querySelector('.removeAntiBody');
 var addedAntiBodyArray = [];
 
 function handleAddAntiBody() {
@@ -52,16 +53,20 @@ addAntiBodyButton.addEventListener('click', handleAddAntiBody);
 
 function handleRemoveAntiBody() {
     var selectedAntiBody = document.querySelector('#addedAntiBody option:checked');
+    addedAntiBodyArray.splice(selectedAntiBody.index, 1);
     document.querySelector('#possibleAntiBody').appendChild(selectedAntiBody);
 }
 
 removeAntiBodyButton.addEventListener('click', handleRemoveAntiBody);
 
-var handleclicksave = function () {
+var inputName = document.querySelector('.name');
+var inputSurname = document.querySelector('.surname');
+function handleclicksave() {
     var saveMessage = document.querySelector('div.alert');
     if (inputName.validity.valid == true && inputSurname.validity.valid == true) {
-        setLocalStorage();
-        updateTable();
+        patients.push(buildNewPatientObject());
+        savePatientsToStorage(patients);
+        updateTable(patients);
         inputName.value = '';
         inputSurname.value = '';
         saveMessage.hidden = true;
@@ -69,9 +74,13 @@ var handleclicksave = function () {
     else {
         saveMessage.hidden = false;
     }
-};
+}
+var saveButton = document.querySelector('.save');
+saveButton.addEventListener('click', handleclicksave);
 
-function setLocalStorage() {
+
+
+function buildNewPatientObject() {
     var sex = document.querySelector('input[name="sex"]:checked').value;
     var bloodType = document.querySelector('input[name="bloodType"]:checked').value;
     var rhesus = document.querySelector('input[name="rhesus"]:checked').value;
@@ -83,36 +92,20 @@ function setLocalStorage() {
     else {
         inputAntiBody = "keine";
     }
-    var patientObject = {
+    return {
         name: inputName.value,
         surname: inputSurname.value,
         antiBody: inputAntiBody,
         sex: sex,
         bloodType: bloodType + " " + rhesus
     };
-    patientStorage.push(patientObject);
-    localStorage.setItem("storedPatientArray", JSON.stringify(patientStorage));
 }
 
-function updateTable() {
-    var text = '';
-    var i;
-    for (i = 0; i < patientStorage.length; i++) {
-        var currentpat = patientStorage[i];
-        text += "<tr><td>" + currentpat.surname + "</td>";
-        text += "<td>" + currentpat.name + "</td>";
-        text += "<td>" + currentpat.sex + "</td>";
-        text += "<td>" + currentpat.bloodType + "</td>";
-        text += "<td>" + currentpat.antiBody + "<span class='glyphicon glyphicon-remove' onclick='removePatient(this)'></span></td></tr>";
-    }
-    patientListBodyElement.innerHTML = text;
 
-}
-saveButton.addEventListener('click', handleclicksave);
+var inputSearch = document.querySelector('input.search');
 
-
-var handlesearch = function () {
-
+function handlesearch() {
+    var patientList = document.querySelector('#patientlist table');
     var rowNr;
 
     for (var rowIndex = 0; rowIndex < patientList.rows.length; rowIndex++) {
@@ -135,7 +128,7 @@ var handlesearch = function () {
         }
     }
 
-};
+}
 inputSearch.addEventListener('keyup', handlesearch);
 
 
@@ -144,14 +137,10 @@ function removePatient(clickedElement) {
     if (ask == true) {
         var td = clickedElement.parentNode;
         var tr = td.parentNode.rowIndex;
-        console.log(tr);
-        var patientStorage = JSON.parse(localStorage.getItem("storedPatientArray"));
         var x = tr - 1;
-        patientStorage.splice(x, 1);
-        localStorage.setItem("storedPatientArray", JSON.stringify(patientStorage));
-        window.location.reload();
-    }
-    else {
+        patients.splice(x, 1);
+        savePatientsToStorage(patients);
+        updateTable(patients);
     }
 }
 
