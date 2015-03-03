82892c5490bda9616c0b49e76bd76fc90a0dba25
Muriele Trentini <muriele@trentini.ch>
2015-03-03 15:35:46 +0100

    adds sort function to AntiBody Lists

--- view.js
+++ view.js
@@ -13,33 +13,33 @@ var view = {
         }
     },
     sortPossibleAntiBodyList: function () {
-        var lb = document.querySelector('#possibleAntiBody');
-        arrTexts = [];
+        var possibleAntiBodyList = document.querySelector('#possibleAntiBody');
+        var antiBodyArray = [];
 
-        for (i = 0; i < lb.length; i++) {
-            arrTexts[i] = lb.options[i].id + ',' + lb.options[i].value + ',' + lb.options[i].text;
+        for (i = 0; i < possibleAntiBodyList.length; i++) {
+            antiBodyArray[i] = possibleAntiBodyList.options[i].id + ',' + possibleAntiBodyList.options[i].value + ',' + possibleAntiBodyList.options[i].text;
         }
-        arrTexts.sort();
-        for (i = 0; i < lb.length; i++) {
-            el = arrTexts[i].split(',');
-            lb.options[i].id = el[0];
-            lb.options[i].value = el[1];
-            lb.options[i].text = el[2];
+        antiBodyArray.sort();
+        for (i = 0; i < possibleAntiBodyList.length; i++) {
+            x = antiBodyArray[i].split(',');
+            possibleAntiBodyList.options[i].id = x[0];
+            possibleAntiBodyList.options[i].value = x[1];
+            possibleAntiBodyList.options[i].text = x[2];
         }
     },
     sortAddedAntiBodyList: function () {
-        var lb = document.querySelector('#addedAntiBody');
-        arrTexts = [];
+        var addedAntiBodyList = document.querySelector('#addedAntiBody');
+        var antiBodyArray = [];
 
-        for (i = 0; i < lb.length; i++) {
-            arrTexts[i] = lb.options[i].id + ',' + lb.options[i].value + ',' + lb.options[i].text;
+        for (i = 0; i < addedAntiBodyList.length; i++) {
+            antiBodyArray[i] = addedAntiBodyList.options[i].id + ',' + addedAntiBodyList.options[i].value + ',' + addedAntiBodyList.options[i].text;
         }
-        arrTexts.sort();
-        for (i = 0; i < lb.length; i++) {
-            el = arrTexts[i].split(',');
-            lb.options[i].id = el[0];
-            lb.options[i].value = el[1];
-            lb.options[i].text = el[2];
+        antiBodyArray.sort();
+        for (i = 0; i < addedAntiBodyList.length; i++) {
+            x = antiBodyArray[i].split(',');
+            addedAntiBodyList.options[i].id = x[0];
+            addedAntiBodyList.options[i].value = x[1];
+            addedAntiBodyList.options[i].text = x[2];
         }
     }
 };
