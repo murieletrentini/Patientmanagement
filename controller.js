c7e5cf939b670fcc90415a9230a5007cb310df30
tarinya <muriele@trentini.ch>
2015-03-22 14:43:04 +0100

    further optimizes: - every queryselector in view.init, - every eventlistener in controller.init, - logic of antibodyselection

--- controller.js
+++ controller.js
@@ -2,47 +2,82 @@
  * Created by muriele on 03.03.15.
  */
 var controller = {
+    init: function() {
+        this.patients = store.getPatients();
+        view.init();
+        view.updateTable(this.patients);
+        view.addAntiBodyButton.addEventListener('click', this.addAntiBodyToSelection);
+        view.removeAntiBodyButton.addEventListener('click', this.removeAntiBodyFromSelection);
+        view.saveButton.addEventListener('click', this.handleClickSave);
+        view.inputSearch.addEventListener('keyup', this.handleSearch);
+    },
     handleSearch: function () {
         var filteredPatients = store.getFilteredPatients(inputSearch.value);
         view.updateTable(filteredPatients);
     },
     handleClickSave: function () {
-        var saveMessage = document.querySelector('div.alert-save');
-        var duplicateMessage = document.querySelector('div.alert-duplicate');
-        if (inputName.validity.valid == true && inputSurname.validity.valid == true) {
-            var duplicatePatientsSurname = store.getFilteredPatients(inputSurname.value);
-            var duplicatePatientsName = store.getFilteredPatients(inputName.value);
+        
+        if (view.inputName.validity.valid == true && view.inputSurname.validity.valid == true) {
+            var duplicatePatientsSurname = store.getFilteredPatients(view.inputSurname.value);
+            var duplicatePatientsName = store.getFilteredPatients(view.inputName.value);
             if (duplicatePatientsSurname.length > 0 && duplicatePatientsName.length > 0) {
-                duplicateMessage.hidden = false;
-                saveMessage.hidden = true;
+                view.duplicateMessageAlert.hidden = false;
+                view.saveMessageAlert.hidden = true;
                 view.updateTable(duplicatePatientsName);
             }
             else {
-                patients.push(model.buildNewPatientObject(inputName, inputSurname, birthDate));
-                store.savePatients(patients);
-                view.updateTable(patients);
-                inputName.value = '';
-                inputSurname.value = '';
-                inputSurname.focus();
-                saveMessage.hidden = true;
-                duplicateMessage.hidden = true;
+                controller.patients.push(controller.buildNewPatientObject());
+                store.savePatients(controller.patients);
+                view.updateTable(controller.patients);
+                view.inputName.value = '';
+                view.inputSurname.value = '';
+                view.inputSurname.focus();
+                view.saveMessageAlert.hidden = true;
+                view.duplicateMessageAlert.hidden = true;
             }
         }
         else {
-            saveMessage.hidden = false;
+            view.saveMessageAlert.hidden = false;
         }
     },
-    onRemovePatient: function onRemovePatient(patientID) {
+    onRemovePatient: function (patientID) {
         var ask = confirm("Soll der Patient wirklich gelöscht werden?");
         if (ask === true) {
             controller.removePatient(patientID);
-            store.savePatients(patients);
-            view.updateTable(patients);
+            store.savePatients(controller.patients);
+            view.updateTable(controller.patients);
         }
     },
-    removePatient: function removePatient(patientID) {
-        _.remove(patients, function predicate(patient) {
+    removePatient: function (patientID) {
+        _.remove(controller.patients, function predicate(patient) {
             return patient.id === patientID;
         });
+    },
+
+    addAntiBodyToSelection: function () {
+        var selectedAntiBody = view.possibleAntiBodySelect.selectedOptions[0];
+        if (selectedAntiBody) {
+            view.addedAntiBodySelect.appendChild(selectedAntiBody);
+            view.sortAddedAntiBodyList()
+        }
+    },
+    removeAntiBodyFromSelection: function () {
+        var selectedAntiBody = view.addedAntiBodySelect.selectedOptions[0];
+        if (selectedAntiBody) {
+            view.possibleAntiBodySelect.appendChild(selectedAntiBody);
+            view.sortPossibleAntiBodyList();
+        }
+    },
+    buildNewPatientObject: function () {
+
+        return {
+            name: view.inputName.value,
+            surname: view.inputSurname.value,
+            antiBody: view.getSelectedAntiBodies(),
+            sex: view.getSelectedSexValue(),
+            birthDate: '',
+            bloodType: view.getSelectedBloodTypeValue() + " " + view.getSelectedRhesusValue(),
+            id: store.getId()
+        };
     }
 };
--- view.js
+++ view.js
@@ -41,14 +41,69 @@ var view = {
             addedAntiBodyList.options[i].value = x[1];
             addedAntiBodyList.options[i].text = x[2];
         }
+    },
+    init: function () {
+        this.inputName = document.querySelector('.name');
+        this.inputSurname = document.querySelector('.surname');
+        this.inputBirthDate = document.querySelector('.birthDate');
+        this.addedAntiBodySelect = document.querySelector('#addedAntiBody');
+        this.possibleAntiBodySelect = document.querySelector('#possibleAntiBody');
+        this.sexRadioButtons = document.querySelectorAll('input[name="sex"]');
+        this.bloodTypeRadioButtons = document.querySelectorAll('input[name="bloodType"]');
+        this.rhesusRadioButtons = document.querySelectorAll('input[name="rhesus"]');
+        this.saveMessageAlert = document.querySelector('div.alert-save');
+        this.duplicateMessageAlert = document.querySelector('div.alert-duplicate');
+        this.addAntiBodyButton = document.querySelector('.addAntiBody');
+        this.removeAntiBodyButton = document.querySelector('.removeAntiBody');
+        this.saveButton = document.querySelector('.save');
+        this.inputSearch = document.querySelector('input.search');
+
+    },
+    getSelectedAntiBodies: function () {
+
+
+        var selectedAntiBodies = _.map(this.addedAntiBodySelect.children, function (optionElement) {
+            return optionElement.value;
+        });
+
+
+        var inputAntiBody;
+        if (selectedAntiBodies.length > 0) {
+            inputAntiBody = selectedAntiBodies.join(', ');
+        }
+        else {
+            inputAntiBody = 'keine';
+        }
+        return inputAntiBody;
+    },
+    getSelectedSexValue: function () {
+        return this.getSelectedRadioButtonValue(this.sexRadioButtons);
+
+    },
+    getSelectedBloodTypeValue: function () {
+        return this.getSelectedRadioButtonValue(this.bloodTypeRadioButtons);
+    },
+    getSelectedRhesusValue: function () {
+        return this.getSelectedRadioButtonValue(this.rhesusRadioButtons);
+    },
+    getSelectedRadioButtonValue: function (radioButtonArray) {
+        var selectedRadio = _.find(radioButtonArray, function (radioButton) {
+            return radioButton.checked;
+        });
+        if (selectedRadio) {
+            return selectedRadio.value;
+        }
+        else {
+            throw new Error('no RadioButton is checked');
+        }
     }
 };
 
 
 $('.form_date').datetimepicker({
-    language:  'de',
+    language: 'de',
     weekStart: 1,
-    todayBtn:  1,
+    todayBtn: 1,
     autoclose: 1,
     todayHighlight: 1,
     startView: 2,
