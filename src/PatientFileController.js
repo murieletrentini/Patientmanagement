5127f16f5cc840aa7d4dbeb5669329dcfe97dccc
Muriele Trentini <muriele@trentini.ch>
2015-06-30 21:42:41 +0200

    integrates addPatient in patientFile

--- src/PatientFileController.js
+++ src/PatientFileController.js
@@ -21,19 +21,63 @@ angular.module('patientmanager').controller('PatientFileController', function (P
     vm.saveCase = saveCase;
     vm.removeCase = removeCase;
     vm.patientAnalyses = '';
-    vm.onSave = onSave;
+    vm.patientData = '';
+    vm.patientSeroError = false;
+    vm.saveSerodiagnosis = saveSerodiagnosis;
+    vm.savePersonalData = savePersonalData;
+//RegEx for ng-pattern of patient name/surname input
+    vm.nameRegex = /[a-zA-Z]+/g;
+    //RegEx for ng-pattern of patient birthdate input
+    vm.birthDateRegex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d\b/;
+
+    vm.isCollapsed = true;
+
 
     if (!_.isUndefined($routeParams.patientId)) {
         var patientId = parseInt($routeParams.patientId);
         vm.patient = vm.patients[patientId];
 
     }
+    
+    
+
+    
 
 
-    function onSave() {
-        buildAntiBodyString();
-        vm.patients[vm.patient.id] = vm.patient;
-        PatientStore.savePatients(vm.patients);
+
+    function savePersonalData() {
+
+        if (!vm.patientData.$valid) {
+            //shows relevant error Messages
+            vm.patientData.errorMsg = true;
+        } else {
+            //gets new id if patient is new i.e. not edited
+            if (!vm.patient.id) {
+                vm.patient.id = PatientStore.getID();
+            }
+            vm.patient.cases = {};
+            vm.patient.antiBodies = [];
+            buildAntiBodyString();
+            vm.patients[vm.patient.id] = vm.patient;
+            PatientStore.savePatients(vm.patients);
+            //hides all previous error Messages
+            vm.patient.errorMsg = false;
+        }
+
+    }
+
+
+
+
+    function saveSerodiagnosis() {
+        if (_.isUndefined(vm.patient.id)) {
+            vm.patientSeroError = true;
+        } else {
+            buildAntiBodyString();
+            vm.patients[vm.patient.id] = vm.patient;
+            PatientStore.savePatients(vm.patients);
+        }
+
     }
 
     function buildAntiBodyString() {
